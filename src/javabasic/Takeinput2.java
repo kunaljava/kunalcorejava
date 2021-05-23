4a75d464d4aab4cd69b5cb4be9ceb2e9fa9c9972
shailendra <shailendra@shailendra-H110M-S2>
2021-05-23 12:41:23 +0530

    input2

---
+++ src/javabasic/Takeinput2.java
@@ -0,0 +1,19 @@
+package javabasic;
+import java.util.Scanner;
+
+public class Takeinput2 {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name:=");
+		String str=sc.next();
+		System.out.println("Enter your father name:=");
+		String str1 =sc.next();
+		System.out.println("Enter your roll no:=");
+		int ro=sc.nextInt();
+		System.out.println("Conggratulation you are promoted to next class");
+		
+		
+	}
+
+}
